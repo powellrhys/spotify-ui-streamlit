611d61a24e9cbbfd3bd9f424c7bb3e223444f1dd
rhys powell <rhys.powell2@btinternet.com>
2024-12-31 09:41:45 +0000

    tuesday files added

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      files: ["**/*.js"],
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      rules: {
+        "no-unused-vars": "warn",
+        "semi": ["error", "always"],
+      },
+    },
+  ];
+  
